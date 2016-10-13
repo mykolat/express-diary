61ad92e92762737523141cb58e41911691f8230e
Mykola Tymchak <mykolanr@gmail.com>
2016-10-13 19:40:43 +0300

    added DB connection

--- app.js
+++ app.js
@@ -5,10 +5,26 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var app = express(),
+    orm = require('orm'),
+    fs = require('fs');
 
-var app = express();
+orm.db = orm.connect("mysql://root:del0@localhost/crypto_diary", function (err, db) {
+    // console.log(db);
+    if (err) {
+        console.log("Something is wrong with the connection", err);
+        return;
+    } else {
+      console.log("connection to DB is ok")
+    }
+});
+
+var modelsPath = __dirname + '/models';
+fs.readdirSync(modelsPath).forEach(function (file) {
+    if (file.indexOf('.js') >= 0) {
+        require(modelsPath + '/' + file);
+    }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +38,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+var routes = require('./routes/index');
+var articles = require('./routes/articles');
+
 app.use('/', routes);
-app.use('/users', users);
+app.use('/articles', articles);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
