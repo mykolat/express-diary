d0a19628c8fe473db2159aabb62a149bc5176a67
Mykola Tymchak <mykolanr@gmail.com>
2016-10-15 14:58:21 +0300

    User registration

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('orm').db;
 var User = db.models.users;
-
+var _ = require('lodash');
 /* GET articles listing. */
 router.get('/', function(req, res, next) {
     User.find(function(err, users) {
@@ -26,20 +26,39 @@ router.get('/:id', function(req, res, next) {
                 login: user.login,
                 email: user.email
             });
-    })
+        })
+});
+
+router.get('/name/:name', function(req, res, next) {
+    User.find({
+            login: req.params.name
+        },
+        function(err, users) {
+            // console.log(user)
+            if (err) throw new Error(err);
+            res.render('users', {
+                layout: 'layouts/other',
+                users: users
+            });
+        });
 });
 
 router.post('/', function(req, res, next) {
-    console.log(req.body)
-    User.create([{
-        title: "Something",
-        url: "demo",
-        text: "hello world"
-    }], function(err, item) {
-        console.log(item)
-        console.log(err)
-        res.sendStatus(200)
-    })
+    if (req.body.password == req.body.password2) {
+        var user = _.pick(req.body, 'login', 'location', 'password', 'email');
+        User.create(user, function(err, item) {
+            if(item) {
+              res.redirect(req.headers.referer)
+            } else {
+              res.send(err.code)
+            }
+                // console.log(err)
+
+        })
+    } else {
+        res.sendStatus(403)
+    }
+
 });
 
 module.exports = router;
